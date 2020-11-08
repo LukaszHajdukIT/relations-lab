858c79c42f148b4dcffba679a3cf90906013e664
LukaszHajdukIT <lukasz.hajduk@interia.com>
2020-11-08 09:23:41 +0100

    first commit

---
+++ relations-lab/src/main/java/hibernate/relations/relationslab/RelationsLabApplication.java
@@ -0,0 +1,13 @@
+package hibernate.relations.relationslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationsLabApplication.class, args);
+	}
+
+}
---
+++ relations-lab/src/test/java/hibernate/relations/relationslab/RelationsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package hibernate.relations.relationslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
