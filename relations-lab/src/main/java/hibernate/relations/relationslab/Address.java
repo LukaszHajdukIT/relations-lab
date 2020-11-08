02b8ced01ff9144abecedb63326694dc8154fefb
LukaszHajdukIT <lukasz.hajduk@interia.com>
2020-11-08 10:02:30 +0100

    added tamplate

---
+++ relations-lab/src/main/java/hibernate/relations/relationslab/Address.java
@@ -0,0 +1,60 @@
+package hibernate.relations.relationslab;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String city;
+    private String streetName;
+    private int houseNo;
+    private String postalCode;
+
+    public Address() {
+    }
+
+    public Address(String city, String streetName, int houseNo, String postalCode) {
+        this.city = city;
+        this.streetName = streetName;
+        this.houseNo = houseNo;
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public int getHouseNo() {
+        return houseNo;
+    }
+
+    public void setHouseNo(int houseNo) {
+        this.houseNo = houseNo;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
---
+++ relations-lab/src/main/java/hibernate/relations/relationslab/AddressRepository.java
@@ -0,0 +1,6 @@
+package hibernate.relations.relationslab;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
